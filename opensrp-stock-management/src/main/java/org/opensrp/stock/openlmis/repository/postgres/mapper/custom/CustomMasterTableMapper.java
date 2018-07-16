6734915723fd6288a1005247a286faf1a2854cc7
Vincent Karuri <vincentmkaruri@gmail.com>
2018-07-16 14:32:23 +0300

    Add custom master table mapper classes

--- opensrp-stock-management/src/main/java/org/opensrp/stock/openlmis/repository/postgres/mapper/custom/CustomMasterTableMapper.java
+++ opensrp-stock-management/src/main/java/org/opensrp/stock/openlmis/repository/postgres/mapper/custom/CustomMasterTableMapper.java
@@ -1,13 +1,15 @@
 package org.opensrp.stock.openlmis.repository.postgres.mapper.custom;
 
 import org.apache.ibatis.annotations.Param;
-import org.opensrp.stock.openlmis.domain.postgres.MasterTable;
-import org.opensrp.stock.openlmis.domain.postgres.MasterTableExample;
-import org.opensrp.stock.openlmis.repository.postgres.mapper.MasterTableMapper;
+import org.opensrp.stock.openlmis.domain.postgres.MasterTableEntry;
+import org.opensrp.stock.openlmis.domain.postgres.MasterTableEntryExample;
+import org.opensrp.stock.openlmis.repository.postgres.mapper.MasterTableEntryMapper;
 
 import java.util.List;
 
-public interface CustomMasterTableMapper extends MasterTableMapper {
+public interface CustomMasterTableMapper extends MasterTableEntryMapper {
 
-    List<MasterTable> select(@Param("example") MasterTableExample example, @Param("offset") int offset, @Param("limit") int limit);
+    List<MasterTableEntry> select(@Param("example") MasterTableEntryExample example, @Param("offset") int offset, @Param("limit") int limit);
+
+    int insertSelectiveAndSetId(MasterTableEntry record);
 }
