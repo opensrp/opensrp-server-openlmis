86f40b2f52db96c8f94e2a2c8590d6336904a85c
Vincent Karuri <vincentmkaruri@gmail.com>
2018-08-02 18:46:05 +0300

    Add OrderableService

---
+++ openlmis-stock/src/main/java/org/opensrp/stock/openlmis/service/OrderableService.java
@@ -0,0 +1,28 @@
+package org.opensrp.stock.openlmis.service;
+
+import org.opensrp.stock.openlmis.domain.Orderable;
+import org.opensrp.stock.openlmis.repository.OrderableRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderableService {
+
+    @Autowired
+    private OrderableRepository repository;
+
+    public List<Orderable> getAll() {
+        return repository.getAll();
+    }
+
+    public List<Orderable> get(long prevServerVersion) {
+        return repository.get(prevServerVersion);
+    }
+
+    // TODO: maybe add update endpoint
+    public void add(Orderable orderable) {
+        repository.add(orderable);
+    }
+}
