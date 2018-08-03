0beb6afa57968104bf1754f18838e75161e98a36
Vincent Karuri <vincentmkaruri@gmail.com>
2018-08-03 11:41:24 +0300

    Add ProgramOrderableService

---
+++ openlmis-stock/src/main/java/org/opensrp/stock/openlmis/service/ProgramOrderableService.java
@@ -0,0 +1,28 @@
+package org.opensrp.stock.openlmis.service;
+
+import org.opensrp.stock.openlmis.domain.ProgramOrderable;
+import org.opensrp.stock.openlmis.repository.ProgramOrderableRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProgramOrderableService {
+
+    @Autowired
+    private ProgramOrderableRepository repository;
+
+    public List<ProgramOrderable> getAll() {
+        return repository.getAll();
+    }
+
+    public List<ProgramOrderable> get(long prevServerVersion) {
+        return repository.get(prevServerVersion);
+    }
+
+    // TODO: maybe add update endpoint
+    public void add(ProgramOrderable orderable) {
+        repository.add(orderable);
+    }
+}
