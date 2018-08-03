8e2631f674a9ece3122e0dad9bada9a555b4e940
Vincent Karuri <vincentmkaruri@gmail.com>
2018-08-03 11:41:50 +0300

    Add ProgramOrderableResource

--- openlmis-stock/src/main/java/org/opensrp/stock/openlmis/rest/ProgramOrderableResource.java
+++ openlmis-stock/src/main/java/org/opensrp/stock/openlmis/rest/ProgramOrderableResource.java
@@ -1,4 +1,87 @@
 package org.opensrp.stock.openlmis.rest;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import org.joda.time.DateTime;
+import org.json.JSONObject;
+import org.opensrp.stock.openlmis.domain.ProgramOrderable;
+import org.opensrp.stock.openlmis.service.ProgramOrderableService;
+import org.opensrp.stock.openlmis.util.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.text.MessageFormat.format;
+import static org.opensrp.stock.openlmis.util.Utils.getLongFilter;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Controller
+@RequestMapping(value = "/rest/program_orderables")
 public class ProgramOrderableResource {
+
+    private static final String LAST_SERVER_VERSION = "last_server_version";
+
+    @Autowired
+    ProgramOrderableService programOrderableService;
+
+    private static Logger logger = LoggerFactory.getLogger(OrderableResource.class.toString());
+    private Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+            .registerTypeAdapter(DateTime.class, new Utils.DateTimeTypeConverter()).create();
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    protected List<ProgramOrderable> getAll() {
+        return programOrderableService.getAll();
+    }
+
+    @RequestMapping(value = "/sync", method = RequestMethod.GET)
+    @ResponseBody
+    protected List<ProgramOrderable> sync(HttpServletRequest request) {
+        try {
+            long serverVersion = getLongFilter(LAST_SERVER_VERSION, request);
+            return programOrderableService.get(serverVersion);
+        } catch (Exception e) {
+            logger.error("", e);
+            return new ArrayList<>();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @RequestMapping(headers = { "Accept=application/json" }, method = POST, value = "/add")
+    public ResponseEntity<HttpStatus> add(@RequestBody String data) {
+
+        try {
+            JSONObject postData = new JSONObject(data);
+            if (!postData.has("program_orderables")) {
+                return new ResponseEntity<>(BAD_REQUEST);
+            }
+
+            List<ProgramOrderable> entries = (ArrayList<ProgramOrderable>) gson.fromJson(postData.getString("program_orderables"),
+                    new TypeToken<ArrayList<ProgramOrderable>>() {}.getType());
+            for (ProgramOrderable entry : entries) {
+                try {
+                    programOrderableService.add(entry);
+                } catch (Exception e) {
+                    logger.error("ProgramOrderable " + entry.getId() == null ? "" : entry.getId() + " failed to sync", e);
+                }
+            }
+        } catch (Exception e) {
+            logger.error(format("Sync data processing failed with exception {0}.- ", e));
+            return new ResponseEntity<>(INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(CREATED);
+    }
 }
