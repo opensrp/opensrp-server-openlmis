fd69392a6009cd9be3f4a13cc1a306d1ad846257
Vincent Karuri <vincentmkaruri@gmail.com>
2018-08-06 14:25:43 +0300

    Add ReasonResource

--- openlmis-stock/src/main/java/org/opensrp/stock/openlmis/rest/ReasonResource.java
+++ openlmis-stock/src/main/java/org/opensrp/stock/openlmis/rest/ReasonResource.java
@@ -1,4 +1,87 @@
 package org.opensrp.stock.openlmis.rest;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import org.joda.time.DateTime;
+import org.json.JSONObject;
+import org.opensrp.stock.openlmis.domain.metadata.ReasonMetaData;
+import org.opensrp.stock.openlmis.service.ReasonService;
+import org.opensrp.stock.openlmis.util.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.text.MessageFormat.format;
+import static org.opensrp.stock.openlmis.util.Utils.SYNC_SERVER_VERSION;
+import static org.opensrp.stock.openlmis.util.Utils.getLongFilter;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Controller
+@RequestMapping(value = "/rest/reasons")
 public class ReasonResource {
+
+    @Autowired
+    ReasonService commodityTypeService;
+    private static Logger logger = LoggerFactory.getLogger(ReasonResource.class.toString());
+    private Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+            .registerTypeAdapter(DateTime.class, new Utils.DateTimeTypeConverter()).create();
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    protected List<ReasonMetaData> getAll() {
+        return commodityTypeService.getAll();
+    }
+
+    @RequestMapping(value = "/sync", method = RequestMethod.GET)
+    @ResponseBody
+    protected List<ReasonMetaData> sync(HttpServletRequest request) {
+        try {
+            long serverVersion = getLongFilter(SYNC_SERVER_VERSION, request);
+            return commodityTypeService.get(serverVersion);
+        } catch (Exception e) {
+            logger.error("", e);
+            return new ArrayList<>();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @RequestMapping(headers = { "Accept=application/json" }, method = POST, value = "/")
+    public ResponseEntity<HttpStatus> add(@RequestBody String data) {
+
+        try {
+            JSONObject postData = new JSONObject(data);
+            if (!postData.has("reasons")) {
+                return new ResponseEntity<>(BAD_REQUEST);
+            }
+
+            List<ReasonMetaData> entries = (ArrayList<ReasonMetaData>) gson.fromJson(postData.getString("reasons"),
+                    new TypeToken<ArrayList<ReasonMetaData>>() {}.getType());
+            for (ReasonMetaData entry : entries) {
+                try {
+                    commodityTypeService.add(entry);
+                } catch (Exception e) {
+                    logger.error("Reason " + entry.getUuid() == null ? "" : entry.getUuid() + " failed to sync", e);
+                }
+            }
+        } catch (Exception e) {
+            logger.error(format("Sync data processing failed with exception {0}.- ", e));
+            return new ResponseEntity<>(INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(CREATED);
+    }
 }
