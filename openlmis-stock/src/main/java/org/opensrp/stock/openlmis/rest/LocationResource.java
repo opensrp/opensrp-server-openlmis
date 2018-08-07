7a5035f3522e63b872175384656181066aa7a0f5
Vincent Karuri <vincentmkaruri@gmail.com>
2018-08-07 18:33:43 +0300

    Add LocationResource

---
+++ openlmis-stock/src/main/java/org/opensrp/stock/openlmis/rest/LocationResource.java
@@ -0,0 +1,26 @@
+package org.opensrp.stock.openlmis.rest;
+
+import org.json.JSONException;
+import org.opensrp.stock.openlmis.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/rest/locations")
+public class LocationResource {
+
+    @Autowired
+    LocationService locationService;
+
+    public static final String LOCATION_ID = "id";
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    protected String getLocationHeirarchy(@PathVariable String id) throws JSONException {
+        return locationService.getLocationHeirarchy(id);
+    }
+}
